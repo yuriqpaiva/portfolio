d7de960a5a566f044e78db898280c6cc11dd7446
Yuri <yuriqpaiva@gmail.com>
2022-07-26 22:02:50 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Text fontSize={40} fontWeight="bold" color="brandRed.500">
+      Hello world!
+    </Text>
+  );
+}
+
+export default App;
