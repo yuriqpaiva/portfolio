f078a3c8b55503f07a36c851ce022769840df6b0
Yuri <yuriqpaiva@gmail.com>
2022-07-29 12:50:00 -0300

    Adjust tooltip position

--- src/components/About/TechnologyItem.tsx
+++ src/components/About/TechnologyItem.tsx
@@ -12,21 +12,22 @@ interface TechnologyItemProps {
 
 export function TechnologyItem({ technology }: TechnologyItemProps) {
   return (
-    <Flex
-      _hover={{ transform: 'translateY(-5px)' }}
-      transition="transform 0.2s ease-in-out"
-      flexDir="column"
-      alignItems="center"
+    <Tooltip
+      label={`${technology.description}`}
+      fontSize="1rem"
+      p="4"
+      bg="brandBlue.700"
     >
-      <Tooltip
-        label={`${technology.description}`}
-        fontSize="1rem"
-        p="4"
-        bg="brandBlue.700"
+      <Flex
+        _hover={{ transform: 'translateY(-5px)' }}
+        transition="transform 0.2s ease-in-out"
+        flexDir="column"
+        alignItems="center"
       >
         <Image src={technology.image_url} alt="" w="14" h="14" />
-      </Tooltip>
-      <Text fontFamily="Roboto Mono" fontWeight="medium" mt="2" opacity={0.8} fontSize="0.875rem">{technology.name}</Text>
-    </Flex>
+        <Text fontFamily="Roboto Mono" fontWeight="medium" mt="2" opacity={0.8} fontSize="0.875rem">{technology.name}</Text>
+      </Flex>
+    </Tooltip>
+
   );
 }
