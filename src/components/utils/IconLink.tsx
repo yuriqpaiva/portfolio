4f1e1c705b1b8bf8da0216065950d857079013b8
Yuri Paiva <yuriqpaiva@gmail.com>
2022-11-01 20:51:35 -0300

    Correct IconLink transition on Safari

--- src/components/utils/IconLink.tsx
+++ src/components/utils/IconLink.tsx
@@ -8,13 +8,16 @@ interface IconLinProps {
 
 export function IconLink({ href, icon }: IconLinProps) {
   return (
-    <Link href={href} isExternal>
+    <Link
+      href={href}
+      isExternal
+      color="gray.300"
+      transition="color 0.2s ease-in-out"
+      _hover={{ color: 'brandRed.500' }}
+    >
       <Icon
         as={icon}
         boxSize="1.75rem"
-        color="gray.300"
-        transition="color 0.2s ease-in-out"
-        _hover={{ color: 'brandRed.500' }}
       />
     </Link>
   );
